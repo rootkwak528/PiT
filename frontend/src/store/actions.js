3fec75026dc632b212e02d5e945dbe6deca964db
yoseph0310 <56868159+yoseph0310@users.noreply.github.com>
2021-08-02 20:44:52 +0900

    :sparkles: [FE] - actions.js : login 구현

--- frontend/src/store/actions.js
+++ frontend/src/store/actions.js
@@ -1,29 +1,31 @@
 // API
 import $axios from 'axios'
 
+// 회원가입
 export function register({ state }, payload) {
   const url = '/users'
   let body = payload
   return $axios.post(url, body)
 }
 
+// 이메일 중복 체크
 export function checkDuplicatedEmail({ state }, payload) {
   const url = `/users/email/${payload.userEmail}`
   return $axios.get(url)
 }
 
+// 닉네임 중복 체크
 export function checkDuplicatedNickname({ state }, payload) {
   const url = `/users/nickname/${payload.userNickname}`
   return $axios.get(url)
 }
-// skeleton 예시 참고
-// export function requestLogin ({ state }, payload) {
-//   console.log('requestLogin', state, payload)
-//   const url = '/auth/login'
-//   let body = payload
-//   return $axios.post(url, body)
-// }
 
+// 로그인
+export function requestLogin({ state }, payload) {
+  const url = `/auth/login`
+  let body = payload
+  return $axios.post(url, body)
+}
 // export function requestUserInfo ({}, token){
 //   console.log('requestUserInfo token : ' + token)
 //   const url = '/api/v1/users/me'
@@ -33,17 +35,3 @@ export function checkDuplicatedNickname({ state }, payload) {
 //     }
 //   })
 // }
-
-// export function requestJoin ({ state }, payload) {
-//   const url = '/users'
-//   let body = payload
-//   return $axios.post(url, body)
-// }
-
-// export function checkDuplicationId ({ state }, payload) {
-//   const url = `/users/${payload.userId}`
-//   return $axios.get(url)
-// }
-// skeleton 예시 참고
-
-
