9547d98839d5c1d7d30458d9b26448dae5f36810
yoseph0310 <56868159+yoseph0310@users.noreply.github.com>
2021-08-02 14:14:22 +0900

    :recycle: [BE] - PitUserDetailsService : @Service 수정

--- backend/src/main/java/com/ssafy/pit/common/auth/PitUserDetailsService.java
+++ backend/src/main/java/com/ssafy/pit/common/auth/PitUserDetailsService.java
@@ -4,12 +4,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
 
 import com.ssafy.pit.entity.User;
 import com.ssafy.pit.service.UserService;
 
 // 현재 액세스 토큰으로부터 인증된 유저의 상세정보(활성화 여브 만료, 롤, 유저정보 등) 관련 서비스 정의
 
+@Service
 public class PitUserDetailsService implements UserDetailsService {
 	
 	@Autowired
