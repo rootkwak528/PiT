c97dd4c2dfe588ae6c9b56f0970718a28a788a7e
yoseph0310 <56868159+yoseph0310@users.noreply.github.com>
2021-08-06 10:30:55 +0900

    [FE] - myInfo.vue : 프로필 사진 추가 css 적용

--- frontend/src/views/mypage/myInfo.vue
+++ frontend/src/views/mypage/myInfo.vue
@@ -444,4 +444,27 @@ export default {
   color: #00c0d4;
   background-color: white;
 }
+.avatar-uploader .el-upload {
+    border: 1px dashed #d9d9d9;
+    border-radius: 6px;
+    cursor: pointer;
+    position: relative;
+    overflow: hidden;
+  }
+  .avatar-uploader .el-upload:hover {
+    border-color: #409eff;
+  }
+  .avatar-uploader-icon {
+    font-size: 28px;
+    color: #8c939d;
+    width: 178px;
+    height: 178px;
+    line-height: 178px;
+    text-align: center;
+  }
+  .avatar {
+    width: 178px;
+    height: 178px;
+    display: block;
+  }
 </style>
