c510b7f8218bc704c5e703b050912c6f58df113f
윤영은 <ounog@naver.com>
2021-08-17 15:35:09 +0900

    :recycle: [FE] update css

--- frontend/src/views/mypage/recordedVideo.vue
+++ frontend/src/views/mypage/recordedVideo.vue
@@ -6,7 +6,7 @@
         <div class="recordedvideo-card-section">
           <el-table
             :data="state.classList"
-            style="width: 80%; font-size: 17px;"
+            style="width: 100%; font-size: 17px;"
             @row-click="mvVideoList"
           >
             <el-table-column prop="classTitle" label="클래스명">
